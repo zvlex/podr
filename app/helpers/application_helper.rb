1fffaf61f954856a40cc9b1a7189deec5ca62e70
Alexander Zutikov <zvlexnum@yandex.com>
2014-12-03 01:16:29 +0400

    add podcast navigation menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def category_list
+    current_user.categories
+  end
+
+  def podcast_list(category)
+    current_user.podcasts.where(category_id: category)
+  end
+
+  def count_unread(obj)
+    obj.feed_items.where(listened: false).count
+  end
 end
--- app/helpers/podcasts_helper.rb
+++ app/helpers/podcasts_helper.rb
@@ -3,7 +3,4 @@ module PodcastsHelper
     obj.feed_items.order('published_at ASC').last.published_at
   end
 
-  def count_unread(obj)
-    obj.feed_items.where(listened: false).count
-  end
 end
